f46dc336624ddb21574e3d041b59d8c4782a7193
ndrwbrwn <92134285+ndrwbrwn@users.noreply.github.com>
2025-12-19 15:32:05 +1000

    finished d6

--- 2024/DaySix.pl
+++ 2024/DaySix.pl
@@ -1,38 +1,72 @@
+use Clone qw/ clone /;
 my @grid;
+my @pci, @pcj;
+my ($starti, $startj);
 
-push @grid, [split('', $_)] while (<>);
+SETUP: {
+	push @grid, [split('', $_)] while (<>);
 
-my ($i, $j);
-SEARCH: for ($i = 0; $i < scalar(@grid); $i++) {
-	for ($j = 0; $j < scalar(@grid); $j++) {
-		last SEARCH if ($grid[$i][$j] eq "^");
+	my ($i, $j);
+	SEARCH: for ($i = 0; $i < scalar(@grid); $i++) {
+		for ($j = 0; $j < scalar(@grid); $j++) {
+			last SEARCH if ($grid[$i][$j] eq "^");
+		}
 	}
+
+	($starti, $startj) = ($i, $j);
 }
 
-my $d = 0;
-my @v;
-my $magic_cross_count = 0;
+PART_ONE: {
+	my ($i, $j, $d) = ($starti, $startj, 0);
+	my @v;
 
-while (@v = movedir($i, $j, $d)) {
-	$d = ($d + 1) % 4;
-	($i, $j) = @v;
+	while (@v = movedir($i, $j, $d)) {
+		$d = ($d + 1) % 4;
+		($i, $j) = @v;
 
-	last if ((!defined $i) || (!defined $j));
-	print "Turned. Going " . $d . " from (" . $i . " , " . $j . ")\n";
-}
-
-my $c;
-for ($i = 0; $i < scalar(@grid); $i++) {
-	for ($j = 0; $j < scalar(@grid); $j++) {
-		$c++ if ($grid[$i][$j] eq "X");
+		last if ((!defined $i) || (!defined $j));
 	}
+
+	for ($i = 0; $i < scalar(@grid); $i++) {
+		for ($j = 0; $j < scalar(@grid); $j++) {
+			push @pc, [($i,$j)] if ($grid[$i][$j] eq "X");
+		}
+	}
+
+	print "Part One\n";
+	print scalar(@pc) . "\n";
 }
 
-print "Part One\n";
-print $c . "\n";
-print "Part Two\n";
-print $magic_cross_count . "\n";
-#6026 too high
+PART_TWO: {
+	my ($loops, $i, $j, $d) = (0, 0, 0, 0);
+
+	foreach my $o (@pc) {
+		my $sq = $grid[@{$o}[0]][@{$o}[1]];
+		$grid[@{$o}[0]][@{$o}[1]] = "#";
+
+		my @turns = ();
+		($i, $j, $d) = ($starti, $startj, 0);
+
+		while (@v = movedir($i, $j, $d)) {
+			$d = ($d + 1) % 4;
+			($i, $j) = @v;
+
+			last if ((!defined $i) || (!defined $j));
+
+			if (grep { @{$_}[0] == $d and @{$_}[1] == $i and @{$_}[2] == $j } @turns) {
+				$loops++;
+				last;
+			}
+
+			push @turns, [($d, $i, $j)];
+		}
+
+		$grid[@{$o}[0]][@{$o}[1]] = $sq;
+	}
+
+	print "Part Two\n";
+	print $loops . "\n";
+}
 
 sub movedir
 {
@@ -40,11 +74,7 @@ sub movedir
 
 	RUN: {
 		if ($d == 2) {
-			my $b = $i;
-			$grid[$b++][$j] = "Y" while (!($grid[$b][$j] eq "#") && $b != scalar(@grid));
-
 			while (!($grid[$i][$j] eq "#")) {
-				$magic_cross_count++ if ($grid[$i][$j] eq "X" || $grid[$i][$j] eq "Y");
 				$grid[$i][$j] = "X";
 				last RUN if ($i == scalar(@grid)-1);
 				$i++;
@@ -53,11 +83,7 @@ sub movedir
 		}
 
 		if ($d == 0) {
-			my $b = $i;
-			$grid[$b--][$j] = "Y" while (!($grid[$b][$j] eq "#") && $b != -1);
-
 			while (!($grid[$i][$j] eq "#")) {
-				$magic_cross_count++ if ($grid[$i][$j] eq "X" || $grid[$i][$j] eq "Y");
 				$grid[$i][$j] = "X";
 				last RUN if ($i == 0);
 				$i--;
@@ -67,11 +93,7 @@ sub movedir
 		}
 
 		if ($d == 1) {
-			my $b = $j;
-			$grid[$i][$b++] = "Y" while (!($grid[$i][$b] eq "#") && $b != scalar(@grid));
-
 			while (!($grid[$i][$j] eq "#")) {
-				$magic_cross_count++ if ($grid[$i][$j] eq "X" || $grid[$i][$j] eq "Y");
 				$grid[$i][$j] = "X";
 				last RUN if ($j == scalar(@grid)-1);
 				$j++;
@@ -81,11 +103,7 @@ sub movedir
 		}
 
 		if ($d == 3) {
-			my $b = $j;
-			$grid[$i][$b--] = "Y" while (!($grid[$i][$b] eq "#") && $b != -1);
-
 			while (!($grid[$i][$j] eq "#")) {
-				$magic_cross_count++ if ($grid[$i][$j] eq "X" || $grid[$i][$j] eq "Y");
 				$grid[$i][$j] = "X";
 				last RUN if ($j == 0);
 				$j--;
